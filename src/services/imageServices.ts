8eebc2d8dd22897472e0f8011f31d24828e07b1a
iHarryD <anuraagvr555@gmail.com>
2022-07-13 20:12:38 +0530

    feat: add edit and delete services for image

--- src/services/imageServices.ts
+++ src/services/imageServices.ts
@@ -58,3 +58,46 @@ export async function postNewImage(
     if (loadingStateSetter) loadingStateSetter(false);
   }
 }
+
+export async function editImage(
+  imageID: string,
+  image: {
+    imageName?: string;
+    imageURL?: string;
+    description?: string;
+  },
+  loadingStateSetter?: Dispatch<SetStateAction<boolean>>,
+  successCallback?: (
+    result: AxiosResponse<{ message: string; data: ImageCard }>
+  ) => void,
+  failureCallback?: (err: unknown) => void
+) {
+  if (loadingStateSetter) loadingStateSetter(true);
+  try {
+    const result = await baseAxiosInstance().put(`/${imageID}/edit`, image);
+    if (successCallback) successCallback(result);
+  } catch (err) {
+    if (failureCallback) failureCallback(err);
+  } finally {
+    if (loadingStateSetter) loadingStateSetter(false);
+  }
+}
+
+export async function deleteImage(
+  imageID: string,
+  loadingStateSetter?: Dispatch<SetStateAction<boolean>>,
+  successCallback?: (
+    result: AxiosResponse<{ message: string; data: ImageCard }>
+  ) => void,
+  failureCallback?: (err: unknown) => void
+) {
+  if (loadingStateSetter) loadingStateSetter(true);
+  try {
+    const result = await baseAxiosInstance().delete(`/delete/${imageID}`);
+    if (successCallback) successCallback(result);
+  } catch (err) {
+    if (failureCallback) failureCallback(err);
+  } finally {
+    if (loadingStateSetter) loadingStateSetter(false);
+  }
+}
