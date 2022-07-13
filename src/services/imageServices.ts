3462220aa8d13e12ff11b570d1cc4d36553e7344
iHarryD <anuraagvr555@gmail.com>
2022-07-13 16:06:18 +0530

    feat: create services for image

---
+++ src/services/imageServices.ts
@@ -0,0 +1,41 @@
+import { AxiosResponse } from "axios";
+import { Dispatch, SetStateAction } from "react";
+import { ImageCard } from "../interfaces/ImageCard.interface";
+import baseAxiosInstance from "./baseAxiosInstance";
+
+export async function getImages(
+  loadingStateSetter?: Dispatch<SetStateAction<boolean>>,
+  successCallback?: (
+    result: AxiosResponse<{ message: string; data: ImageCard[] }>
+  ) => void,
+  failureCallback?: (err: unknown) => void
+) {
+  if (loadingStateSetter) loadingStateSetter(true);
+  try {
+    const result = await baseAxiosInstance().get("");
+    if (successCallback) successCallback(result);
+  } catch (err) {
+    if (failureCallback) failureCallback(err);
+  } finally {
+    if (loadingStateSetter) loadingStateSetter(false);
+  }
+}
+
+export async function postNewImage(
+  image: ImageCard,
+  loadingStateSetter?: Dispatch<SetStateAction<boolean>>,
+  successCallback?: (
+    result: AxiosResponse<{ message: string; data: ImageCard }>
+  ) => void,
+  failureCallback?: (err: unknown) => void
+) {
+  if (loadingStateSetter) loadingStateSetter(true);
+  try {
+    const result = await baseAxiosInstance().post("/", image);
+    if (successCallback) successCallback(result);
+  } catch (err) {
+    if (failureCallback) failureCallback(err);
+  } finally {
+    if (loadingStateSetter) loadingStateSetter(false);
+  }
+}
