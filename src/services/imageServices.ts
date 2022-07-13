36fb0e3a5d7c9e5ad0b951a67d7697535030b8e9
iHarryD <anuraagvr555@gmail.com>
2022-07-13 17:13:12 +0530

    feat: add single image get service

--- src/services/imageServices.ts
+++ src/services/imageServices.ts
@@ -21,6 +21,25 @@ export async function getImages(
   }
 }
 
+export async function getImage(
+  imageID: string,
+  loadingStateSetter?: Dispatch<SetStateAction<boolean>>,
+  successCallback?: (
+    result: AxiosResponse<{ message: string; data: ImageCard }>
+  ) => void,
+  failureCallback?: (err: unknown) => void
+) {
+  if (loadingStateSetter) loadingStateSetter(true);
+  try {
+    const result = await baseAxiosInstance().get(`/${imageID}`);
+    if (successCallback) successCallback(result);
+  } catch (err) {
+    if (failureCallback) failureCallback(err);
+  } finally {
+    if (loadingStateSetter) loadingStateSetter(false);
+  }
+}
+
 export async function postNewImage(
   image: ImageCard,
   loadingStateSetter?: Dispatch<SetStateAction<boolean>>,
