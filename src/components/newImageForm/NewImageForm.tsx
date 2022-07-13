6195930b4aaa4c72e103536ecad091f53c3890f9
iHarryD <anuraagvr555@gmail.com>
2022-07-13 16:07:49 +0530

    feat: add image fetching and posting functionality

--- src/components/newImageForm/NewImageForm.tsx
+++ src/components/newImageForm/NewImageForm.tsx
@@ -1,20 +1,59 @@
+import { FormEvent, useRef } from "react";
+import { postNewImage } from "../../services/imageServices";
 import commonStyles from "../../style/Common.module.css";
 import newImageFormStyles from "./NewImageForm.module.css";
 
 export default function NewImageForm() {
+  const imageNameInputRef = useRef<HTMLInputElement | null>(null);
+  const imageDescriptionInputRef = useRef<HTMLInputElement | null>(null);
+  const imageURLInputRef = useRef<HTMLInputElement | null>(null);
+
+  function handleSubmitForm(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (
+      imageNameInputRef.current === null ||
+      imageDescriptionInputRef.current === null ||
+      imageURLInputRef.current === null
+    )
+      return;
+    if (
+      !imageNameInputRef.current.value.replaceAll(" ", "") ||
+      !imageDescriptionInputRef.current.value.replaceAll(" ", "") ||
+      !imageURLInputRef.current.value.replaceAll(" ", "")
+    )
+      console.log("Empty");
+    const newImage = {
+      imageName: imageNameInputRef.current.value,
+      imageDetails: {
+        description: imageDescriptionInputRef.current.value,
+        uploadedOn: new Date(),
+        lastModifiedOn: new Date(),
+      },
+      imageURL: imageURLInputRef.current.value,
+    };
+    postNewImage(newImage);
+  }
+
   return (
-    <form className={newImageFormStyles["new-image-form"]}>
+    <form
+      className={newImageFormStyles["new-image-form"]}
+      onSubmit={(e) => handleSubmitForm(e)}
+    >
       <div className={commonStyles["input-label-container"]}>
         <label htmlFor="imageName">Image Name</label>
-        <input name="imageName" id="imageName" />
+        <input ref={imageNameInputRef} name="imageName" id="imageName" />
       </div>
       <div className={commonStyles["input-label-container"]}>
         <label htmlFor="imageURL">Image URL</label>
-        <input name="imageURL" id="imageURL" />
+        <input ref={imageURLInputRef} name="imageURL" id="imageURL" />
       </div>
       <div className={commonStyles["input-label-container"]}>
-        <label htmlFor="imageDetails">Image Details</label>
-        <input name="imageDetails" id="imageDetails" />
+        <label htmlFor="imageDescription">Image Description</label>
+        <input
+          ref={imageDescriptionInputRef}
+          name="imageDescription"
+          id="imageDescription"
+        />
       </div>
       <div>
         <button className={commonStyles["primary-btn"]}>Post</button>
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,30 @@
+import { useEffect, useState } from "react";
 import ImageCard from "../../components/imageCard/ImageCard";
+import { ImageCard as IImageCard } from "../../interfaces/ImageCard.interface";
+import { getImages } from "../../services/imageServices";
 import homeStyles from "./Home.module.css";
 
 export default function Home() {
-  return <main className={homeStyles["image-cards-container"]}></main>;
+  const [images, setImages] = useState<IImageCard[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    getImages(setIsLoading, (result) => setImages(result.data.data));
+  }, []);
+
+  return (
+    <main className={homeStyles["image-cards-container"]}>
+      {isLoading ? (
+        <h3>Loading...</h3>
+      ) : (
+        images.map(({ imageDetails, imageName, imageURL }: IImageCard) => (
+          <ImageCard
+            imageDetails={imageDetails}
+            imageName={imageName}
+            imageURL={imageURL}
+          />
+        ))
+      )}
+    </main>
+  );
 }
